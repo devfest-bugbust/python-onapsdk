78352a34ee988aa97cdab1b490bf4d7353c1a3bc
chardon1 <32737664+chardon1@users.noreply.github.com>
2021-09-21 11:56:06 +0200

    update default value

--- src/onapsdk/msb/k8s/definition.py
+++ src/onapsdk/msb/k8s/definition.py
@@ -123,9 +123,9 @@ class Definition(DefinitionBase):
         return cls(
             definition["rb-name"],
             definition["rb-version"],
-            definition.get("chart-name", None),
-            definition.get("description", None),
-            definition.get("labels", None)
+            definition.get("chart-name", "no chart-name"),
+            definition.get("description", "no description"),
+            definition.get("labels", "no label")
         )
 
     @classmethod
